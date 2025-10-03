b49614f7bc8ab5685421ca6b3d6051c423efea09
Zeeshan Asghar <mzeeshanff10@gmail.com>
2025-10-03 21:25:15 +0400

    feat(server): add validation, exception handling & response interceptor

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { AppModule } from "@/app.module";
+import cookieParser from "cookie-parser";
+import { GlobalValidationPipe } from "@/common/pipes/validation.pipe";
+import { AllExceptionsFilter } from "@/common/filters/exceptions.filter";
+import { ResponseInterceptor } from "@/common/interceptors/response.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  app.use(cookieParser());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalPipes(GlobalValidationPipe);
+  await app.listen(4000);
 }
 bootstrap();
