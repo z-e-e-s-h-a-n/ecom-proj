23fa91f95757ed02b7ea32b09cf33b8a194a15da
Zeeshan Asghar <mzeeshanff10@gmail.com>
2025-10-04 06:56:49 +0400

    fix(server/prisma): update prisma module setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { EnvModule } from "./modules/env/env.module";
 import { validateEnv } from "@/common/schemas/env.schema";
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { validateEnv } from "@/common/schemas/env.schema";
       isGlobal: true,
       validate: validateEnv,
     }),
+    PrismaModule,
   ],
 })
 export class AppModule {}
